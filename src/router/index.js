2dc79aec0bd66b78ede15dbab5dcd3af8382568d
Guohao8 <2062885783@qq.com>
2020-02-23 17:36:00 +0800

    master

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,32 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
+const Home = () => import('views/home/Home')
+const Cart = () => import('views/cart/Cart')
+const Category = () => import('views/category/Category')
+const Porfile = () => import('views/porfile/Porfile')
+
 const routes = [
-  
+  {
+    path:'',
+    redirect: '/home'
+  },
+  {
+    path:'/home',
+    component: Home
+  },
+  {
+    path:'/cart',
+    component: Cart
+  },
+  {
+    path:'/category',
+    component: Category
+  },
+  {
+    path:'/porfile',
+    component: Porfile
+  }
 ]
 
 const router = new VueRouter({
---
+++ src/views/home/home.vue
@@ -0,0 +1,39 @@
+<template>
+<div id="home">
+  <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+</div>
+</template>
+
+<script>
+
+import NavBar from 'components/common/navbar/NavBar'
+import {getHomeMultidata} from 'network/Home'
+
+export default {
+  name:"home",
+  components:{
+    NavBar
+  },
+  data() {
+    return {
+      banners:[],
+      recommends:[],
+      dKeywords:[],
+      Keywordss:[],
+    }
+  },
+  created(){
+    getHomeMultidata().then(res => {
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+    })
+  }
+}
+</script>
+
+<style>
+.home-nav{
+ background-color: var(--color-tint);
+ color: white
+}
+</style>
