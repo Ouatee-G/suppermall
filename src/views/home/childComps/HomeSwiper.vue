49cd7a4e2b5aa162fa84995454c8d5fbe4033bbe
Guohao8 <2062885783@qq.com>
2020-02-29 16:54:11 +0800

    master

--- src/views/home/childComps/HomeSwiper.vue
+++ src/views/home/childComps/HomeSwiper.vue
@@ -3,7 +3,7 @@
   <swiper>
     <swiper-item v-for="item in banners">
       <a :href="item.link">
-        <img :src="item.image">
+        <img :src="item.image" @load="imageLoad">
       </a>
     </swiper-item>
   </swiper>
@@ -22,11 +22,23 @@ export default {
       }
     }
   },
+  data() {
+    return {
+      isLoad: false
+    }
+  },
   components:{
     Swiper,
     SwiperItem
   },
-
+  methods: {
+    imageLoad(){
+      if(!this.isLoad){
+        this.$emit('swiperImageLoad')
+        this.isLoad = true
+      }
+    }
+  },
 }
 </script>
 
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,18 +1,23 @@
 <template>
 <div id="home">
   <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <tab-control :titles = "['流行','新款','精选']"
+                  @tabClick = "tabClick"
+                  ref="tabControl1"
+                  class="tab-control"
+                  v-show="isTabFixed"/>
     <scroll class="content"
             ref="scroll"
             :probe-type="3"
             @scroll="contentScroll"
             :pull-up-load="true"
             @pullingUp="loadMore">
-      <home-swiper :banners = "banners"/>
+      <home-swiper :banners = "banners" @swiperImageLoad="swiperImageLoad"/>
       <recommend-view :recommends = "recommends"/>
       <feature-view></feature-view>
-      <tab-control class="tab-control"
-                    :titles = "['流行','新款','精选']"
-                     @tabClick = "tabClick"/>
+      <tab-control :titles = "['流行','新款','精选']"
+                     @tabClick = "tabClick"
+                     ref="tabControl2"/>
       <goods-list :goods = "showGoods"/>
     </scroll>
     <back-top @click.native = "backClick" v-show="isShowBackTop"/>
@@ -34,6 +39,7 @@ import BackTop from 'components/content/backtop/BackTop'
 
 
 import {getHomeMultidata,getHomeGoods} from 'network/Home'
+import {debounce} from 'common/utils'
 
 export default {
   name:"home",
@@ -58,6 +64,8 @@ export default {
       },
       currentType:'pop',
       isShowBackTop:false,
+      tabOffsetTop: 0,
+      isTabFixed: false
     }
   },
   computed: {
@@ -65,16 +73,25 @@ export default {
       return this.goods[this.currentType].list
     }
   },
+  activated() {
+    this.$refs.scroll.scrollTo(0,this.saveY,0)
+    this.$refs.scroll.refresh()
+  },
+  deactivated() {
+    this.saveY = this.$refs.scroll.getScrollY()
+  },
   created(){
     this.getHomeMultidata(),
 
     this.getHomeGoods('pop')
     this.getHomeGoods('new')
     this.getHomeGoods('sell')
+
   },
   mounted() {
+    const refresh = debounce(this.$refs.scroll.refresh,100)
     this.$bus.$on('itemImageLoad',() => {
-      this.$refs.scroll.refresh()
+      refresh()
     })
   },
   methods: {
@@ -93,15 +110,21 @@ export default {
           this.currentType = 'sell'
           break
       }
+      this.$refs.tabControl1.currentIndex = index;
+      this.$refs.tabControl2.currentIndex = index;
     },
     backClick(){
       this.$refs.scroll.scrollTo(0,0)
     },
     contentScroll(position){
-      this.isShowBackTop = (-position.y > 1000 )
+      this.isShowBackTop = (-position.y) > 1000
+      this.isTabFixed = (-position.y) > this.tabOffsetTop
     },
     loadMore(){
       this.getHomeGoods(this.currentType)
+    },
+    swiperImageLoad(){
+     this.tabOffsetTop = this.$refs.tabControl2.$el.offsetTop
     },
     /**
      * 网络请求的方法
@@ -126,23 +149,12 @@ export default {
 
 <style  scoped>
 #home{
-  padding-top: 44px;
   height: 100vh;
   position: relative;
 }
 .home-nav{
  background-color: var(--color-tint);
  color: white;
- position: fixed;
- left: 0;
- right: 0;
- top: 0;
- z-index: 10;
-}
-.tab-control{
-  position: sticky;
-  top: 44px;
-  z-index: 10;
 }
 .content{
   overflow: hidden;
@@ -151,5 +163,10 @@ export default {
   bottom: 49px;
   left: 0;
   right: 0;
+}
+.tab-control{
+  position: relative;
+  z-index: 8;
+  margin-top: -5px
 }
 </style>
