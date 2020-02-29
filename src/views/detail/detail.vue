088f642c719f75ac87a5d07eab78689e793295f4
Guohao8 <2062885783@qq.com>
2020-02-29 22:20:51 +0800

    master

--- src/views/detail/detail.vue
+++ src/views/detail/detail.vue
@@ -2,36 +2,41 @@
   <div class="detail">
     <detail-nav-bar></detail-nav-bar>
     <detail-swiper :top-images = "topImages"></detail-swiper>
+    <detail-base-info :goods="goods"></detail-base-info>
   </div>
 </template>
 
 <script>
   import DetailNavBar from './childComps/DetailNavBar'
   import DetailSwiper from './childComps/DetailSwiper'
+  import DetailBaseInfo from './childComps/DetailBaseInfo'
 
-  import {getDetail} from 'network/detail'
+  import {getDetail, Goods} from 'network/detail'
   export default {
     name: "Detail",
     components:{
       DetailNavBar,
-      DetailSwiper
+      DetailSwiper,
+      DetailBaseInfo
     },
     data() {
       return {
         iid: null,
-        topImages: []
+        topImages: [],
+        goods: null
       }
     },
     created() {
       this.iid = this.$route.params.iid
 
       getDetail(this.iid).then(res => {
-        this.topImages = res.result.itemInfo.topImages
+        const data = res.result
+        this.topImages = data.itemInfo.topImages
+        this.goods = new Goods(data.itemInfo,data.columns,data.shopInfo.services)
       })
     },
   }
 </script>
 
 <style scoped>
-
 </style>
